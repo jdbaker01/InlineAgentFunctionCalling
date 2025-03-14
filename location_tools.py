c584d6b3881d28562394b7514a0011f504df15de
nfolkert <nfolkert@foursquare.com>
2025-03-13 20:49:55 -0400

    Update the Foursquare API version

--- location_tools.py
+++ location_tools.py
@@ -7,20 +7,17 @@ import httpx
 from function_calls import bedrock_agent_tool, get_bedrock_tools
 import json
 
-FSQ_UNVERSIONED_API_BASE = "https://api.foursquare.com"
 FSQ_V20241206_API_BASE = "https://places-api.foursquare.com"
 
 FSQ_SERVICE_TOKEN = os.getenv("FOURSQUARE_SERVICE_TOKEN")
 
-def submit_request(endpoint: str, params: dict[str, str], version20241206: bool) -> str:
+def submit_request(endpoint: str, params: dict[str, str]) -> str:
     headers = {
         "Authorization": f"Bearer {FSQ_SERVICE_TOKEN}",
+        "X-Places-Api-Version": "2025-02-05"
     }
-    if version20241206:
-        headers["X-Places-Api-Version"] = "2024-12-06"
     encoded_params = urlencode(params)
-    url_base = FSQ_V20241206_API_BASE if version20241206 else FSQ_UNVERSIONED_API_BASE
-    url = f"{url_base}{endpoint}?{encoded_params}"
+    url = f"{FSQ_V20241206_API_BASE}{endpoint}?{encoded_params}"
     with httpx.Client() as client:
         try:
             print(url)
@@ -56,7 +53,7 @@ def search_near(what: str, where: str=None, ll: str=None, radius: int=1600) -> s
         params["ll"] = ll
         params["radius"] = radius
 
-    return submit_request("/places/search", params, version20241206=True)
+    return submit_request("/places/search", params)
 
 @bedrock_agent_tool(action_group="LocationToolsActionGroup")
 def get_location() -> str:
@@ -83,7 +80,7 @@ def place_from_latitude_and_longitude(ll: str) -> str:
         "ll": ll,
         "limit": 1
     }
-    return submit_request("/v3/places/nearby", params, version20241206=False)
+    return submit_request("/geotagging/candidates", params)
 
 
 @bedrock_agent_tool(action_group="LocationToolsActionGroup")
@@ -91,7 +88,7 @@ def place_details(fsq_place_id: str) -> str:
     """
         Get detailed information about a place based on the fsq_id (foursquare id), including:
        description, phone, website, social media, hours, popular hours, rating (out of 10),
-       price, menu, top photos, top tips (reviews from users), top tastes, and features
+       price, menu, top photos, top tips (reviews from users), top tastes, and attributes
        such as takes reservations.
     Args:
         fsq_place_id: foursquare id (foursquare id) of the place which is returned from search_near.
@@ -99,10 +96,10 @@ def place_details(fsq_place_id: str) -> str:
     """
 
     params = {
-      "fields": "description,tel,website,social_media,hours,hours_popular,rating,price,menu,photos,tips,tastes,features"
+      "fields": "description,tel,website,social_media,hours,hours_popular,rating,price,menu,photos,tips,tastes,attributes"
     }
 
-    return submit_request(f"/v3/places/{fsq_place_id}", params, version20241206=False)
+    return submit_request(f"/places/{fsq_place_id}", params)
 
 
 
